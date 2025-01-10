ad708bc260853f0eb7db577504c82073ad0d2b10
oscar <oscarmancilla227@gmail.com>
2025-01-10 12:24:05 -0600

    implementacion de completeTask

--- app/src/main/java/com/oscar/todoapp/initialScreen/data/network/InitialScreenClient.kt
+++ app/src/main/java/com/oscar/todoapp/initialScreen/data/network/InitialScreenClient.kt
@@ -4,14 +4,20 @@ import com.oscar.todoapp.initialScreen.data.models.Task
 import com.oscar.todoapp.initialScreen.data.network.response.InitialScreenResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
 
 interface InitialScreenClient {
 
 
 
-    @GET("tarea/")
+    @GET("tarea/getTasks")
     suspend fun getTasks(): Response<List<Task>>
+
+    @PATCH("tarea/complete/")
+    suspend fun completeTask(@Query("id") idTask: Int): Response<String>
 
 
 }
