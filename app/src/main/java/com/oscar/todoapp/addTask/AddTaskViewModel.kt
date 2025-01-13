3116eef3c14602a706244950aa2cb7158ca9ea6b
oscar <oscarmancilla227@gmail.com>
2025-01-13 16:20:42 -0600

    viewModel variables

--- app/src/main/java/com/oscar/todoapp/addTask/AddTaskViewModel.kt
+++ app/src/main/java/com/oscar/todoapp/addTask/AddTaskViewModel.kt
@@ -1,9 +1,23 @@
 package com.oscar.todoapp.addTask
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.oscar.todoapp.addTask.domain.AddTaskUseCase
 import javax.inject.Inject
 
 class AddTaskViewModel @Inject constructor(private val addTaskUseCase: AddTaskUseCase){
+
+
+    private val _nombre = MutableLiveData<String>();
+    var nombre: LiveData<String> = _nombre;
+
+    private val _descripcion = MutableLiveData<String>();
+    var descripcion: LiveData<String> = _descripcion;
+
+    private val _fecha = MutableLiveData<String>();
+    var fecha: LiveData<String> = _fecha;
+
+
 
 
 
